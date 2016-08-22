d72cf010c5bce7cc5394d26cc14d80c98b470ac1
jakoblo <jakoblo@Jakobs-Air.fritz.box>
2016-08-22 19:22:06 +0200

    reducer fix

--- src/js/reducerIndex.js
+++ src/js/reducerIndex.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux'
 import { appReducer } from './reducers/appReducer'
 import filesystem from './filesystem/fs-index'
 import { configReducer } from './reducers/configReducer'
-import Navbar from '../navbar/navbar-reducer'
+import Navbar from './navbar/navbar-index'
 import { viewContainerReducer } from './reducers/view-container-reducer'
 import { List, Map } from 'immutable'
 
@@ -10,6 +10,6 @@ export const rootReducer = combineReducers({
   app: appReducer,
   [filesystem.constants.NAME]: filesystem.reducer,
   config: configReducer,
-  navbar: navbarReducer,
+  navbar: Navbar.reducer,
   viewContainer: viewContainerReducer
 }) 
