3c8f6bb30e8a08840f798b3d7869414fa6c0ff3b
jakoblo <jakoblo@me.com>
2016-10-24 08:16:35 +0200

    fix

--- src/js/file-item/fi-index.js
+++ src/js/file-item/fi-index.js
@@ -1,10 +1,8 @@
 import * as actions from './fi-actions';
 import * as eventHandler from './fi-event-handler/fi-event-handler-index'
 import fileItem from './components/file-item'
-import fileItemRename from './components/file-item-rename'
 
 export default {actions, eventHandler, components: {
-  fileItem: fileItem,
-  fileItemRename: fileItemRename
+  fileItem: fileItem
 }}
 
