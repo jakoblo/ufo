486bd40b2f8220f6ec9dbfc340a11234d918cbfb
Sebastian <sebastian@shapedesign.rocks>
2017-01-18 21:18:32 +0100

    fix windows symlinks 2

--- src/js/filesystem/watch/fs-watch-actions.js
+++ src/js/filesystem/watch/fs-watch-actions.js
@@ -6,6 +6,7 @@ import App from '../../app/app-index'
 const watcherSettings = {
   persistent: true,
   depth: 0,
+  followSymlinks: false, // Symlinks are annoying on windows
   alwaysStat: true
 }
 /**
