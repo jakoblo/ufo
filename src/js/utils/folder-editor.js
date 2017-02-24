24f8d2270a7a14e1916bb6b7aeb556f62c74568a
jakoblo <jakoblo@me.com>
2017-02-24 17:58:34 +0100

    fix

--- src/js/utils/folder-editor.js
+++ src/js/utils/folder-editor.js
@@ -10,13 +10,6 @@ export function saveFileToDisk(basePath, data) {
 }
 
 export function loadFilefromDisk(basePath) {
-    fs.writeFile(basePath + "/ufo.json", data, function (err) {
-        if (err) {
-            return false
-        }
-        return true
-    });
-
     fs.readFile(basePath + '/ufo.json', (err, data) => {
         if (err) throw err;
         console.log(data);
