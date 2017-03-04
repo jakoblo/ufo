49940704cc919070e2ae443fd47a2bf3a549c7ba
Sebastian Wolf <sebastian@shapedesign.rocks>
2017-03-04 20:54:11 +0100

    use electron app.getFileIcon

--- src/js/view-file/render/render-unavailable.js
+++ src/js/view-file/render/render-unavailable.js
@@ -1,15 +1,37 @@
 import React from 'react'
 
+import electron from 'electron'
+const {app} = electron.remote
 export default class DisplayUnavailable extends React.Component {
 
   constructor(props) {
     super(props)
+    this.state = {
+      previewImage: null
+    }
+    this.requestIcon(this.props.path)
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if(this.props.path != nextProps.path) {
+      this.requestIcon(nextProps.path)
+    }
+  }
+
+  requestIcon = (path) => {
+    console.log(path)
+    app.getFileIcon(path, (error, image) => {
+      this.setState({
+        previewImage: 'data:image/png;base64,' + image.toPNG().toString('base64')
+      })
+    })
   }
   
   render() {
+
     return(
       <div className="render-unavailable">
-          Sorry, no preview possible 
+          <img src={this.state.previewImage} />
       </div>
     )
   }
