f18914d66ede2a2d1baf028b0beff928c0b283af
Sebastian <sebastian@shapedesign.rocks>
2017-05-22 12:38:18 +0200

    Light theme (#47)
    * Dark Red theme 1
    
    * layout improvements
    
    * dark red 2
    
    * Light Theme

--- src/js/filesystem/write/fs-write-reducer.js
+++ src/js/filesystem/write/fs-write-reducer.js
@@ -7,6 +7,85 @@ import type { Action } from "../../types";
 
 const INITIAL_STATE = Map({});
 
+// const INITIAL_STATE = fromJS({
+//   "0": {
+//     id: 0,
+//     clobber: false,
+//     type: "COPY",
+//     sources: [
+//       "/Users/flow/Pictures/vlcsnap-2017-01-25-11h11m35s126.png",
+//       "/Users/flow/Pictures/vlcsnap-2017-02-11-00h25m05s252.png",
+//       "/Users/flow/Pictures/vlcsnap-2017-02-11-00h31m07s45.png"
+//     ],
+//     target: "/Users/flow/Pictures/new Folder",
+//     subTasks: {},
+//     errors: {},
+//     finished: true
+//   },
+//   "1": {
+//     id: 1,
+//     clobber: false,
+//     type: "COPY",
+//     sources: [
+//       "/Users/flow/Pictures/Screen Shot 2016-11-08 at 23.41.26.png",
+//       "/Users/flow/Pictures/Screen Shot 2016-11-09 at 01.09.24 (2).png",
+//       "/Users/flow/Pictures/Screen Shot 2016-11-09 at 01.09.24.png",
+//       "/Users/flow/Pictures/alien-226245_1280.jpg",
+//       "/Users/flow/Pictures/cloud-20046_1920.jpg"
+//     ],
+//     target: "/Users/flow/Pictures/new Folder",
+//     subTasks: {},
+//     errors: {},
+//     finished: true
+//   },
+//   "2": {
+//     id: 2,
+//     clobber: false,
+//     type: "COPY",
+//     sources: [
+//       "/Users/flow/Pictures/vlcsnap-2017-01-25-11h11m35s126.png",
+//       "/Users/flow/Pictures/vlcsnap-2017-02-11-00h25m05s252.png",
+//       "/Users/flow/Pictures/vlcsnap-2017-02-11-00h31m07s45.png"
+//     ],
+//     target: "/Users/flow/Pictures/new Folder",
+//     subTasks: {
+//       "/Users/flow/Pictures/new Folder/vlcsnap-2017-01-25-11h11m35s126.png": {
+//         id: 2,
+//         source: "/Users/flow/Pictures/vlcsnap-2017-01-25-11h11m35s126.png",
+//         destination: "/Users/flow/Pictures/new Folder/vlcsnap-2017-01-25-11h11m35s126.png",
+//         type: "COPY",
+//         percentage: 1,
+//         clobber: false
+//       },
+//       "/Users/flow/Pictures/new Folder/vlcsnap-2017-02-11-00h25m05s252.png": {
+//         id: 2,
+//         source: "/Users/flow/Pictures/vlcsnap-2017-02-11-00h25m05s252.png",
+//         destination: "/Users/flow/Pictures/new Folder/vlcsnap-2017-02-11-00h25m05s252.png",
+//         type: "COPY",
+//         percentage: 1,
+//         clobber: false
+//       },
+//       "/Users/flow/Pictures/new Folder/vlcsnap-2017-02-11-00h31m07s45.png": {
+//         id: 2,
+//         source: "/Users/flow/Pictures/vlcsnap-2017-02-11-00h31m07s45.png",
+//         destination: "/Users/flow/Pictures/new Folder/vlcsnap-2017-02-11-00h31m07s45.png",
+//         type: "COPY",
+//         percentage: 1,
+//         clobber: false
+//       }
+//     },
+//     errors: {
+//       EEXIST: [
+//         {
+//           code: "EEXIST",
+//           path: "/Users/flow/Pictures/new Folder/vlcsnap-2017-01-25-11h11m35s126.png"
+//         }
+//       ]
+//     },
+//     finished: false
+//   }
+// });
+
 export default function reducer(
   state: any = INITIAL_STATE,
   action: Action = { type: "", payload: {} }
